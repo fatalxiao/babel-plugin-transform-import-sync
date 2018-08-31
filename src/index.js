dd0c6693aaa3e3d54f59e994449011efb0986a06
liangxiaojun <fatalxiao@163.com>
2018-08-31 10:33:27 +0800

    update src

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
-const syntax = require('babel-plugin-syntax-dynamic-import');
+import {declare} from '@babel/helper-plugin-utils';
+import syntax from '@babel/plugin-syntax-dynamic-import';
 
-module.exports = function ({template, types}) {
+// module.exports = function ({template, types}) {
+//
+//     const buildImport = template('\
+//         (function() {const component = require(SOURCE); return component.default || component;})()\
+//     ');
+//
+//     return {
+//
+//         inherits: syntax,
+//
+//         visitor: {
+//             Import(path) {
+//
+//                 const importArguments = path.parentPath.node.arguments,
+//                     isString = types.isStringLiteral(importArguments[0]) || types.isTemplateLiteral(importArguments[0]);
+//
+//                 if (isString) {
+//                     types.removeComments(importArguments[0]);
+//                 }
+//
+//                 if (path.parentPath) {
+//                     path.parentPath.replaceWith(buildImport({
+//                         SOURCE: isString ?
+//                             importArguments
+//                             :
+//                             types.templateLiteral([
+//                                 types.templateElement({raw: '', cooked: ''}),
+//                                 types.templateElement({raw: '', cooked: ''}, true)
+//                             ], importArguments)
+//                     }));
+//                 }
+//
+//             }
+//         }
+//
+//     };
+// };
 
-    const buildImport = template('\
+export default declare((api, options) => {
+
+    api.assertVersion(7);
+
+    const buildImport = api.template('\
         (function() {const component = require(SOURCE); return component.default || component;})()\
     ');
 
     return {
-
         inherits: syntax,
-
         visitor: {
             Import(path) {
 
@@ -34,6 +73,6 @@ module.exports = function ({template, types}) {
 
             }
         }
-
     };
-};
+
+});
